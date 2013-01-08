2044fe2a9e6833fc88de2ee0d033ab04da7caf5f
Bogaert Cyrille <cyrille@toxicode.fr>
2013-01-08 11:54:00 -0500

    Initialized function created

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-/* Thumbs usage
-<!-- include before all javascript -->
-<script type="text/javascript" src="thumbs.js"></script>
 
-// later on...
-
-element.addEventListener('touchstart', callback, false);
-
-// or an attribute...
-
-element.setAttribute('ontouchstart', 'console.log("hi");');
-
-// or an inline attribute...
-
-<a href="#" ontouchstart="console.log('hi');">Hi</a>
-*/
+var Draw = {};
+function initialize() {
+    Draw.canvas = document.getElementById("drawing_canvas");
+    Draw.canvas.addEventListener('touchstart', function() {
+        alert("touched");
+    }, false);
+}
+window.onload = initialize;
